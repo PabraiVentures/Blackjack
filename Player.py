244ecb85ab2133cabbf0e50b759b8cff9ad1d56d
Nathan Pabrai <nathanpabrai@gmail.com>
2015-01-14 23:15:06 -0500

    Created player with get decision function

---
+++ Player.py
@@ -0,0 +1,28 @@
+from Card import *
+
+class Player:
+	def __init__(self,id):
+		self.id=id
+		self.hands=[] #a player's hands is a list of hands a player has
+			      #a particular hand is a list of cards in one hand
+			      # self.hands for example might have 2 hands => [ [J:diamonds, 2:hearts] , [J:hearts, A:spades]  ]
+		self.bets=[] #bets correspond to hands of same index
+		self.money=1000
+	def getPlayerDecisionsForHand(self,handIdx): #gets one decision for one hand
+		goodInput = False
+		while(goodInput == False):
+			
+			if(len(self.hands[handIdx]) == 2):
+				if((self.hands[handIdx][0].value() == self.hands[handIdx][1].value()) &&(self.bets[handIdx] <= self.money ) ):
+					#if same value and we have enough cash we can split
+					response = raw_input("Player "+str(self.id)+ " has $"+ str(self.money) + ". Hand bet is $"+str(self.bets[handIdx])+".\nEnter your decision to choose between 'split', 'double', 'hit' and 'stand'\n")
+					goodInput = response in ['split,'double','hit','stand']
+				else:
+					response = raw_input("Player "+str(self.id)+ " has $"+ str(self.money) + ". Hand bet is $"+str(self.bets[handIdx])+".\nEnter your decision to choose between 'hit' and 'stand'\n")
+                                        goodInput = response in ['hit','stand']
+			else:
+				 response = raw_input("Player "+str(self.id)+ " has $"+ str(self.money) + ". Hand bet is $"+str(self.bets[handIdx])+".\nEnter your decision to choose between 'hit' and 'stand'\n")
+                                 goodInput = response in ['hit','stand']
+
+		return response
+
